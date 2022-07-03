ee6d76c29e91d3c11c3cdc95176374510cea963b
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-03 22:40:48 +0300

    Task7

---
+++ Task7/Task7/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintNumberSimpleFactors(68);
+        }
+
+        //<summary>
+        //Вывести на экран, из каких простых множителей состоит введенное натуральное число.
+        //</summary>
+        static void PrintNumberSimpleFactors(int number)
+        {
+            if (IsSimple(number))
+            {
+                Console.WriteLine(number);
+                return;
+            }
+
+            while (number != 1)
+            {
+                for (int numberFact = 2; numberFact <= number / 2; numberFact++)
+                {
+                    if (number % numberFact == 0 && IsSimple(numberFact))
+                    {
+                        Console.WriteLine(numberFact);
+                        number = number / numberFact;
+                        break;
+                    }
+                }
+
+                if (IsSimple(number))
+                {
+                    Console.WriteLine(number);
+                    return;
+                }
+            }
+        }
+
+        static bool IsSimple(int numberToCheck)
+        {
+            for (int checkSimple = 2; checkSimple <= numberToCheck / 2; checkSimple++)
+            {
+                if (numberToCheck % checkSimple == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
