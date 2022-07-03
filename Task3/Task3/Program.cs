e25db9cb6b2e86a2936117a5b273e022fc6da6cc
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-03 12:16:38 +0300

    Task3

---
+++ Task3/Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintMultiplicationTable(9);
+        }
+
+        //<summary>
+        //Print multiplication table
+        //<summary>
+
+        static void PrintMultiplicationTable(int tableSize)
+        {
+            for (int firstFactor = 1; firstFactor <= tableSize; firstFactor++)
+            {
+                for (int secondFactor = 1; secondFactor <= tableSize; secondFactor++)
+                {
+                    Console.Write(firstFactor * secondFactor + "  ");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
