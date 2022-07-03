aeea54f676ffbde4aae6637c3cf5f509d9a8cea7
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-04 00:41:00 +0300

    Task8

---
+++ Task8/Task8/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindPerfectNumbers(1000);
+        }
+
+        //<summary>
+        //number is perfect, if the sum of its dividers, except itself, equals itself
+        //examle: 6
+        //1 + 2 + 3 = 6
+        //</summary>
+        static void FindPerfectNumbers(int range)
+        {
+            int dividersSum;
+            for (int checkNumber = 1; checkNumber <= range; checkNumber++)
+            {
+                dividersSum = 1;
+
+                for (int checkDivider = 2; checkDivider <= checkNumber / 2; checkDivider++)
+                {
+                    if (checkNumber % checkDivider == 0)
+                    {
+                        dividersSum += checkDivider;
+                    }
+                }
+
+                if (dividersSum == checkNumber)
+                {
+                    Console.WriteLine(checkNumber);
+                }
+            }
+        }
+    }
+}
