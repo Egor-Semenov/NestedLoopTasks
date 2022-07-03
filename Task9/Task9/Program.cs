e81216e48d173a6af12437ea28192066990b4d7b
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-04 01:05:21 +0300

    Task9

---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintRectangle(20, 10, '#', '*');
+        }
+
+        //<summary>
+        //Вывести на экран «прямоугольник», образованный из двух видов символов.
+        //Контур прямоугольника должен состоять из одного символа, а в «заливка» — из другого.
+        //</summary>
+
+        static void PrintRectangle(int length, int width, char outSideCharacter, char inSideCharacter)
+        {
+            for (int row = 1; row <= length; row++)
+            {
+                if (row == 1 || row == length)
+                {
+                    for (int column = 1; column <= width; column++)
+                    {
+                        Console.Write($"{outSideCharacter}");
+                    }
+                }
+                else
+                {
+                    Console.Write($"{outSideCharacter}");
+
+                    for (int column = 2; column < width; column++)
+                    {
+                        Console.Write($"{inSideCharacter}");
+                    }
+
+                    Console.Write($"{outSideCharacter}");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
