d462d0abb36b0a70fbee18f015e9d268f45198e5
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-03 11:17:10 +0300

    Task1

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,7 +6,42 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PrintPattern(10);
+        }
+       
+        //<summary>
+        //Print pattern
+        //*
+        //**
+        //***
+        //****
+        //***
+        //**
+        //*
+        //</summary>
+
+        static void PrintPattern(int size)
+        {
+            for (int i = 1; i <= size; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
+
+            for (int i = size - 1; i > 0; i--)
+            {
+                for (int j = size - 1; j >= size - i; j--) 
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine("");
+            }
         }
     }
 }
+
