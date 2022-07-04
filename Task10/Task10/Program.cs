f258184f9e4be50e214b4eb77b1339f3b7f5d0d8
Egor.Siamionau <egor2002semenov@gmail.com>
2022-07-04 12:45:42 +0300

    Task10

---
+++ Task10/Task10/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[,] { { 11, 25, 30 }, { 47, 52, 63}, { 78, 81, 90 }, { 18, 29, 13 } };
+            PrintMatrix(matrix);
+            Console.WriteLine("");
+            FindColumnsSum(matrix);
+        }
+
+        //<summary>
+        //Задана матрица неотрицательных чисел.
+        //Посчитать сумму элементов в каждом столбце.
+        //</summary>
+        static void FindColumnsSum(int[,] matrix)
+        {
+            int rowCount = matrix.GetUpperBound(0) + 1;
+            int columnCount = matrix.Length / (matrix.GetUpperBound(0) + 1);
+
+            int columnSum;
+
+            for (int column = 0; column < columnCount; column++)
+            {
+                columnSum = 0;
+
+                for (int row = 0; row < rowCount; row++)
+                {
+                    columnSum += matrix[row, column];
+                }
+
+                Console.Write($"{columnSum} ");
+            }
+        }
+
+        static void PrintMatrix(int[,] matrix)
+        {
+            int rowCount = matrix.GetUpperBound(0) + 1;
+            int columnCount = matrix.Length / (matrix.GetUpperBound(0) + 1);
+
+            for (int row = 0; row < rowCount; row++)
+            {
+                for (int column = 0; column < columnCount; column++) 
+                {
+                    Console.Write($"{matrix[row, column]} ");
+                }
+
+                Console.WriteLine("");
+            }
+        }
+    }
+}
